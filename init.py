08bc660be09ce4db144a9a2f8efb2edd630396a7
n <n@n.com>
2025-04-11 22:44:32 +0800

    Automated commit

--- init.py
+++ init.py
@@ -15,7 +15,7 @@ def git_commit_and_remove_history():
         # 添加所有文件
         subprocess.run(['git', 'add', '-A'], check=True)
         # 提交新的初始提交
-        subprocess.run(['git', 'commit', '-m', 'init'], check=True)
+        subprocess.run(['git', 'commit', '-m', '"init"'], check=True)
         # 删除原来的主分支
         subprocess.run(['git', 'branch', '-D', 'main'], check=True)
         # 将新分支重命名为原来的主分支
